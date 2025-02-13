54a2bbeba5897d37f649fae17036e45c6973a62b
AdelleStuuu <adelledoesstuff@gmail.com>
2025-02-13 11:36:41 +0800

    added sfx

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let currentIndex = 0 ;
 const images = ["Images/Adelle Sprites/1.png", "Images/Adelle Sprites/2.png", "Images/Adelle Sprites/3.png","Images/Adelle Sprites/4.png"];
 const text = ["test","We haven't done a lot these past days... But just so you know, I still love you from the bottom of my heart","Im looking forward to our movie date, but I hope you're looking forward to what I have in store on our anniversary!","Soo, Would like to be my Valentine?"]
-
 function changeImage(direction) {
     if (direction === 'next') {
         currentIndex = (currentIndex + 1); // Move to the next image
@@ -21,6 +20,8 @@ function sheChoseYes() {
     document.getElementById('speechBubble').innerHTML = "YIPPEEE !!! See you on friday night!!!";
     document.getElementById('yesButton').style.visibility = 'hidden';
     document.getElementById('noButton').style.visibility = 'hidden';
+    var audio = document.getElementById("audio");
+      audio.play(); 
 }
 
 function sheChoseNo() {
